eef812daf6d45775e4ee7c91ad62e561990f4f6c
Pierrot62 <courquin.pierre62@gmail.com>
2021-12-07 16:24:14 +0100

    Edit Main

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace GestionCantine
         {
             InitializeComponent();
         }
+
+        private void OpenWindow(object sender, RoutedEventArgs e)
+        {
+            string NameWindow = ((Button)sender).Content;
+        }
     }
 }
