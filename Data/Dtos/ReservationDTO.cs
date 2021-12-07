9c1c3b024f21b7422c6f8087648e784a1626436f
Quentin BALAIR <quentin.balair2706@gmail.com>
2021-12-07 15:13:43 +0100

    reservation DTO et commencement profile

--- Data/Dtos/ReservationDTO.cs
+++ Data/Dtos/ReservationDTO.cs
@@ -8,5 +8,23 @@ namespace GestionCantine.Data.Dtos
 {
     class ReservationDTOOut
     {
+        public int IdReservation { get; set; }
+        public int? IdEleve { get; set; }
+        public int? IdMenu { get; set; }
+        public string LibelleMenu { get; set; }
+
+        public string NomEleve { get; set; }
+        public string PrenomEleve { get; set; }
+        public DateTime DDNEleve { get; set; }
+        public DateTime? DateReservation { get; set; }
+
     }
+
+    class ReservationDTOIn
+    {
+        public DateTime DateReservation { get; set; }
+        public int idEleve { get; set; }
+        public int idMenu { get; set; }
+    }
+
 }
