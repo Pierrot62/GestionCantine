8a4ac22243b0ed4efa61b734a4a3ee4ce5754ef8
nilsKolliker <nils.kolliker@gmail.com>
2021-12-07 14:44:55 +0100

    test

---
+++ Data/Profiles/PaiementProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using GestionCantine.Data.Dtos;
+using GestionCantine.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCantine.Data.Profiles
+{
+    class PaiementProfile : Profile
+    {
+        public PaiementProfile()
+        {
+            CreateMap<PaiementDTOIn, Paiement>();
+            CreateMap<Paiement, PaiementDTOIn>();
+            //CreateMap<Paiement, PaiementDTOOut>().ForMember(d => d.NomEleve, o => o.MapFrom(s => s.));
+        }
+    }
+}
