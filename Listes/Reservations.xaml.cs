7ead7282664d1f422479e8021cb009cf7d457aab
Quentin BALAIR <quentin.balair2706@gmail.com>
2021-12-07 18:51:28 +0100

    Remplissage liste reservation

--- Listes/Reservations.xaml.cs
+++ Listes/Reservations.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GestionCantine.Controllers;
+using GestionCantine.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,25 +21,33 @@ namespace GestionCantine.Listes
     /// </summary>
     public partial class Reservations : Window
     {
-        public MainWindow FenetreMere { get; set; }
-        public Reservations(MainWindow FenetreMere)
+        private MainWindow FenetreMere { get; set; }
+        private ReservationController _ReservationController { get; set; }
+
+        public Reservations(MainWindow FenetreMere, GCantineContext _ctx)
         {
             InitializeComponent();
             this.FenetreMere = FenetreMere;
+            this._ReservationController = new ReservationController(_ctx);
             Init();
         }
 
         private void Init()
         {
-            
+            dg.ItemsSource = _ReservationController.GetAllReservation();
         }
 
 
 
         private void Back(object sender, RoutedEventArgs e)
-        { 
-            this.Close();
+        {
+            if (this.Left != this.FenetreMere.Left || this.Top != this.FenetreMere.Top)
+            {
+                this.FenetreMere.Left = this.Left;
+                this.FenetreMere.Top = this.Top;
+            }
             this.FenetreMere.Visibility = Visibility.Visible;
+            this.Close();
         }
     }
 }
