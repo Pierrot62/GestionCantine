eb0c8d7d673a8a76d52c123f389e351a193b8aae
Pierrot62 <courquin.pierre62@gmail.com>
2021-12-07 14:19:12 +0100

    Init

---
+++ Data/Models/Elefe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace GestionCantine.Data.Models
+{
+    public partial class Elefe
+    {
+        public Elefe()
+        {
+            Paiements = new HashSet<Paiement>();
+            Reservations = new HashSet<Reservation>();
+        }
+
+        public int IdEleve { get; set; }
+        public string NomEleve { get; set; }
+        public string PrenomEleve { get; set; }
+        public DateTime? Ddneleve { get; set; }
+        public double? SoldeEleve { get; set; }
+
+        public virtual ICollection<Paiement> Paiements { get; set; }
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
---
+++ Data/Models/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace GestionCantine.Data.Models
+{
+    public partial class Reservation
+    {
+        public int IdReservation { get; set; }
+        public int? IdEleve { get; set; }
+        public int? IdMenu { get; set; }
+        public DateTime? DateReservation { get; set; }
+
+        public virtual Elefe IdEleveNavigation { get; set; }
+        public virtual Menu IdMenuNavigation { get; set; }
+    }
+}
